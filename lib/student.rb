0514b42df21f9fda12b69acb72d15f86194a6b05
Ron Sala <ron.sala@gmail.com>
2018-05-27 05:42:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,16 @@
 require 'pry'
 
 class Student
-#   attr_accessor :id, :name, :grade
-#
-#   def self.new_from_db(row)
-#     student = self.new
-#     student.id = row[0]
-#     student.name = row[1]
-#     student.grade = row[2]
-#     student
-#   end
-#
+  attr_accessor :id, :name, :grade
+
+  def self.new_from_db(row)
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end
+
 #   def self.find_by_name(name)
 #     # find the student in the database given a name
 #     # return a new instance of the Student class
